d58c414eb2b0d7c745b993630c59700ed9f0b2d9
Josh Bawtinheimer <jbawtinheimer@gmail.com>
2020-08-26 19:50:17 +0000

    refactored play js file

--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log('you ded cuz you idled');
-  });
-
-  return conn;
-}
-
-console.log('Connecting ...');
+const connect = require('./client');
+console.log('Connecting...');
 connect();
